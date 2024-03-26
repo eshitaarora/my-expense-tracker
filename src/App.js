15c96991a83c9086b10ea8d73939ffe1a24d4327
Keerthana <pekeerthana@deloitte.com>
2024-03-26 15:16:10 +0530

    expense list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import Home from './pages/Home';
-import Navbar from './pages/Navbar';
- 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Navbar from "./pages/Navbar";
+import ExpenseList from "./components/employee/ExpenseList";
+
 function App() {
   return (
     <Router>
       <Routes>
-          <Route exact path="/"  element={<Login/>} />
-          <Route path="/register"  element={<Register/>} />
-          <Route path="/dashboard"  element={<Dashboard/>} />
-          <Route path="/home"  element={<Home/>} />
-          <Route path="/navbar"  element={<Navbar/>} />
+        <Route exact path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/expenselist" element={<ExpenseList />} />
+        <Route path="/navbar" element={<Navbar />} />
       </Routes>
     </Router>
   );
 }
-  
+
 export default App;
