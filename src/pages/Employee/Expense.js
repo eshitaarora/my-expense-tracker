6b084545674d3b89ca16198b8ab314df0b08ff7b
Keerthana <pekeerthana@deloitte.com>
2024-03-27 10:47:09 +0530

    New expense

--- src/pages/Employee/Expense.js
+++ src/pages/Employee/Expense.js
@@ -36,146 +36,143 @@ const Expense = () => {
         console.error("Error storing value:", error);
         // Handle error case
       });
-
-    return (
-      <>
-        <Layout>
-          <div className="row justify-content-md-center">
-            <h1>Add expenses</h1>
-            <form>
-              <ul>
-                <label className="id" id="id">
-                  EmployeeId
-                </label>
-                <> </>
-                <input
-                  type="number"
-                  className="id"
-                  id="id"
-                  value={employeeId}
-                  onChange={(e) => setEmployeeId(e.target.value)}
-                ></input>
-              </ul>
-
-              <ul>
-                <label for="datetime" className="date" id="date">
-                  DateOfRequest
-                </label>
-                <> </>
-                <input
-                  type="datetime-local"
-                  className="date"
-                  id="date"
-                  value={dateOfRequest}
-                  onChange={(e) => setDateOfRequest(e.target.value)}
-                ></input>
-              </ul>
-
-              <ul>
-                {" "}
-                <label className="type" id="type">
-                  ExpenseType
-                </label>
-                <> </>
-                <input
-                  type="text"
-                  className="type"
-                  id="type"
-                  value={expenseType}
-                  onChange={(e) => setExpenseType(e.target.value)}
-                ></input>
-              </ul>
-
-              <ul>
-                <label className="desc" id="desc">
-                  ExpenseDescription
-                </label>
-                <> </>
-                <input
-                  type="text"
-                  className="desc"
-                  id="desc"
-                  value={expenseDescription}
-                  onChange={(e) => setExpenseDescription(e.target.value)}
-                ></input>
-              </ul>
-
-              <ul>
-                <label className="desc" id="desc">
-                  Status
-                </label>
-                <> </>
-                <input
-                  type="text"
-                  className="desc"
-                  id="desc"
-                  value={status}
-                  onChange={(e) => setStatus(e.target.value)}
-                ></input>
-              </ul>
-
-              <ul>
-                <label className="amt" id="amt">
-                  Amount
-                </label>
-                <> </>
-                <input
-                  type="number"
-                  className="amt"
-                  id="amt"
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value)}
-                ></input>
-              </ul>
-
-              <ul>
-                <label className="man" id="man">
-                  ManagerName
-                </label>
-                <> </>
-                <input
-                  type="text"
-                  className="man"
-                  id="man"
-                  value={managerName}
-                  onChange={(e) => setManagerName(e.target.value)}
-                ></input>
-              </ul>
-              <ul>
-                <label className="man" id="man">
-                  ManagerId
-                </label>
-                <> </>
-                <input
-                  type="text"
-                  className="man"
-                  id="man"
-                  value={managerId}
-                  onChange={(e) => setManagerId(e.target.value)}
-                ></input>
-              </ul>
-
-              <button
-                style={{
-                  backgroundColor: "grey",
-                  textAlign: "center",
-                  fontWeight: "bold",
-                }}
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Submit
-              </button>
-            </form>
-          </div>
-        </Layout>
-      </>
-    );
   };
+
+  return (
+    <>
+      <Layout>
+        <div className="row justify-content-md-center">
+          <h1>Add expenses</h1>
+          <form>
+            <ul>
+              <label className="id" id="id">
+                EmployeeId
+              </label>
+              <> </>
+              <input
+                type="number"
+                className="id"
+                id="id"
+                value={employeeId}
+                onChange={(e) => setEmployeeId(e.target.value)}
+              ></input>
+            </ul>
+
+            <ul>
+              <label for="datetime" className="date" id="date">
+                DateOfRequest
+              </label>
+              <> </>
+              <input
+                type="datetime-local"
+                className="date"
+                id="date"
+                value={dateOfRequest}
+                onChange={(e) => setDateOfRequest(e.target.value)}
+              ></input>
+            </ul>
+
+            <ul>
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="Type">ExpenseType</label>
+                  <select>
+                    <option>ChooseType</option>
+                    <option>Travel</option>
+                    <option>Food</option>
+                    <option>Health/Medical</option>
+                    <option>Accessories</option>
+                    <option>Miscellanious</option>
+                  </select>
+                </div>
+              </div>
+            </ul>
+
+            <ul>
+              <label className="desc" id="desc">
+                ExpenseDescription
+              </label>
+              <> </>
+              <input
+                type="text"
+                className="desc"
+                id="desc"
+                value={expenseDescription}
+                onChange={(e) => setExpenseDescription(e.target.value)}
+              ></input>
+            </ul>
+
+            <ul>
+              <label className="desc" id="desc">
+                Status
+              </label>
+              <> </>
+              <input
+                type="text"
+                className="desc"
+                id="desc"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              ></input>
+            </ul>
+
+            <ul>
+              <label className="amt" id="amt">
+                Amount
+              </label>
+              <> </>
+              <input
+                type="number"
+                className="amt"
+                id="amt"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              ></input>
+            </ul>
+
+            <ul>
+              <label className="man" id="man">
+                ManagerName
+              </label>
+              <> </>
+              <input
+                type="text"
+                className="man"
+                id="man"
+                value={managerName}
+                onChange={(e) => setManagerName(e.target.value)}
+              ></input>
+            </ul>
+            <ul>
+              <label className="man" id="man">
+                ManagerId
+              </label>
+              <> </>
+              <input
+                type="text"
+                className="man"
+                id="man"
+                value={managerId}
+                onChange={(e) => setManagerId(e.target.value)}
+              ></input>
+            </ul>
+
+            <button
+              style={{
+                backgroundColor: "grey",
+                textAlign: "center",
+                fontWeight: "bold",
+              }}
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </Layout>
+    </>
+  );
 };
 
-{
-  /* <label className="status" id="status">StatusOfRequest</label>
-                    <input type="text" className="status" id="status"></input> */
-}
 export default Expense;
